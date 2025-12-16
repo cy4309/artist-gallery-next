89618c06832f78cf3fcf2b8081edf6e1b4065649
Chester Chu <chester@spe3d.co>
2025-12-16 17:05:26 +0800

    fix: FavoriteButton imageUrl

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -43,7 +43,7 @@ const Carousel = ({
   pauseOnHover = false,
   round = false,
 }: CarouselProps) => {
-  const orgWebsiteUrl = "https://cloud.culture.tw"; //line只接受https，即使org有些圖片是http
+  // const orgWebsiteUrl = "https://cloud.culture.tw"; //line只接受https，即使org有些圖片是http
   const isSingle = items.length === 1; // 🔥 單筆判斷（最重要）
 
   const [dynamicWidth, setDynamicWidth] = useState(baseWidth);
@@ -255,7 +255,11 @@ const Carousel = ({
                   <FavoriteButton
                     eventId={String(item.actId)}
                     eventTitle={item.actName}
-                    imageUrl={orgWebsiteUrl + item.imageUrl}
+                    // imageUrl={orgWebsiteUrl + item.imageUrl}
+                    imageUrl={
+                      process.env.NEXT_PUBLIC_BASE_URL +
+                      getCultureImageUrl(item.imageUrl)
+                    } //line只吃https
                     dateText={`${item.startTime.split(",")[0]} - ${
                       item.endTime.split(",")[0]
                     }`}
