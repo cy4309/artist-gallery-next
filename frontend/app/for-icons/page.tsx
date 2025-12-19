282dd31cdba4de0865a9db4d949dedf14ce78572
Chester Chu <chester@spe3d.co>
2025-12-19 15:20:47 +0800

    add: route for-icons

---
+++ frontend/app/for-icons/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarDays,
+  faHeart,
+  faNewspaper,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function TestPage() {
+  return (
+    <div className="w-full h-full flex">
+      <div className="w-60 h-60 max-w-2xl border border-gray-800 flex flex-col justify-center items-center font-nunito gap-1 mx-auto">
+        <FontAwesomeIcon
+          icon={faCalendarDays}
+          className="text-6xl text-[#3b82f6]"
+        />
+        <h2>View Activities</h2>
+        <h2>查看活動</h2>
+      </div>
+
+      <div className="w-60 h-60 max-w-2xl border border-gray-800 flex flex-col justify-center items-center font-nunito gap-1 mx-auto">
+        <FontAwesomeIcon icon={faHeart} className="text-6xl text-[#3b82f6]" />
+        <h2>Saved</h2>
+        <h2>查看收藏</h2>
+      </div>
+
+      <div className="w-60 h-60 max-w-2xl border border-gray-800 flex flex-col justify-center items-center font-nunito gap-1 mx-auto">
+        <FontAwesomeIcon
+          icon={faNewspaper}
+          className="text-6xl text-[#3b82f6]"
+        />
+        <h2>Zine Column</h2>
+        <h2>查看專欄</h2>
+      </div>
+    </div>
+  );
+}
